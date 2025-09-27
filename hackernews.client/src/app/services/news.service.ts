c7ba2584a56024175849ca578616970e8de8f48c
Christian Lundblad <ctlundblad@gmail.com>
2025-09-27 01:15:07 -0400

    fixed environments issue since angular environments arent being replaced when build config changes

--- hackernews.client/src/app/services/news.service.ts
+++ hackernews.client/src/app/services/news.service.ts
@@ -7,15 +7,17 @@ import { Injectable } from '@angular/core';
 export class NewsService {
   constructor(private http: HttpClient) {}
 
+  baseUrl = 'https://localhost:7278';
+
   getLatestNews(pageNumber: number, pageSize: number) {
     return this.http.get(
-      `https://localhost:7278/api/news?pagenumber=${pageNumber}&pagesize=${pageSize}`,
+      `${this.baseUrl}/api/news?pagenumber=${pageNumber}&pagesize=${pageSize}`,
       { observe: 'response' }
     );
   }
   getRankedNews(pageNumber: number, pageSize: number, searchString: string) {
     return this.http.get(
-      `https://localhost:7278/api/news/search?searchString=${searchString}&pagenumber=${pageNumber}&pagesize=${pageSize}`,
+      `${this.baseUrl}/api/news/search?searchString=${searchString}&pagenumber=${pageNumber}&pagesize=${pageSize}`,
       { observe: 'response' }
     );
   }
